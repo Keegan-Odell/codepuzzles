33c2ea123aa6e3025906a45cc32dcdba87431964
Keegan Odell <Keegan.Odell@icloud.com>
2023-07-19 18:17:47 -0500

    finished problem 4

--- codewars-js-1.js
+++ codewars-js-1.js
@@ -31,4 +31,32 @@ function solution(num) {
 	);
 }
 
+// A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+// For example, take 153 (3 digits), which is narcissistic:
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+// This may be True and False in your language, e.g. PHP.
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+function narcissistic(value) {
+	let valueString = value.toString();
+	let lengthOfBase = valueString.length;
+
+	let poweredStuff = 
+    Array.from(value.toString())
+		.map((num) => Math.pow(num, lengthOfBase))
+		.reduce((acc, cur) => acc + cur, 0);
+
+    return poweredStuff === value
+}
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+function filter(array) {
+  return array.filter((ele) => typeof(ele) !== 'string')
+}
+
+console.log(filter([1,2,'a','b']))
 
