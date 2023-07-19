c8da43aefbebd5aeeef7c36fb26edd774698a067
Keegan Odell <Keegan.Odell@icloud.com>
2023-07-19 17:41:46 -0500

    finished second problem

--- codewars-js-1.js
+++ codewars-js-1.js
@@ -3,15 +3,32 @@
 // Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
 // Note: The function accepts an integer and returns an integer.
 function squareDigits(int) {
-  let squaredNumArray = []
-  let numArray = Array.from(int.toString())
-  for(nums of numArray) {
-    let num = Math.pow(nums, 2)
-    squaredNumArray.push(num)
-  }
-  return Number(squaredNumArray.join(''))
+	let squaredNumArray = [];
+	let numArray = Array.from(int.toString());
+	for (nums of numArray) {
+		let num = Math.pow(nums, 2);
+		squaredNumArray.push(num);
+	}
+	return Number(squaredNumArray.join(''));
 }
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
 // Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(num) {
+	let arrayof3_5 = [];
+	for (let i = 1; i < num; i++) {
+		if (i % 3 === 0) {
+			arrayof3_5.push(i);
+		} else if (i % 5 === 0) {
+			arrayof3_5.push(i);
+		}
+	}
+	return arrayof3_5.reduce(
+		(accumulator, currentValue) => accumulator + currentValue,
+		0
+	);
+}
+
+
