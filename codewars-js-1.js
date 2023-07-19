a54f938c22dfdd5585ae2462d7b20b98963a95de
Keegan Odell <Keegan.Odell@icloud.com>
2023-07-19 17:20:15 -0500

    finished first problem

--- codewars-js-1.js
+++ codewars-js-1.js
@@ -1,9 +1,17 @@
 // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
-
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
-
 // Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
-
 // Note: The function accepts an integer and returns an integer.
+function squareDigits(int) {
+  let squaredNumArray = []
+  let numArray = Array.from(int.toString())
+  for(nums of numArray) {
+    let num = Math.pow(nums, 2)
+    squaredNumArray.push(num)
+  }
+  return Number(squaredNumArray.join(''))
+}
 
-// Happy Coding!
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Note: If the number is a multiple of both 3 and 5, only count it once.
