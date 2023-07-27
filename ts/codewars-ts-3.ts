290f794dc7973c7b7655538e691c11940691afeb
Keegan Odell <Keegan.Odell@icloud.com>
2023-07-26 21:12:27 -0500

    solved new problem

--- ts/codewars-ts-3.ts
+++ ts/codewars-ts-3.ts
@@ -31,4 +31,26 @@ export const digitalRoot = (n: number): number => {
   }
 };
 
-console.log(digitalRoot(456));
+//Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+//
+// For example (Input --> Output):
+//
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+export const persistence = (num: number): number => {
+  let loopCounter: number = 0;
+  while (num >= 10) {
+    num = Number(
+      Array.from(num.toString()).reduce(
+        (acc, val) => Number(acc) * Number(val),
+        1,
+      ),
+    );
+    loopCounter += 1;
+  }
+  return loopCounter;
+};
+
+console.log(persistence(25));
